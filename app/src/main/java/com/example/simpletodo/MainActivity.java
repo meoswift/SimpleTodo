e9e3761caf702884b3f6dea6d182f5031051361c
Linh Tran <ltran10@fb.com>
2020-06-23 17:01:05 -0700

    Successfully implemented read and write from files function to keep the todo list persistent

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -5,11 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import org.apache.commons.io.FileUtils;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,9 +37,10 @@ public class MainActivity extends AppCompatActivity {
         item_input = findViewById(R.id.item_input);
         items_rv = findViewById(R.id.items_list);
 
-        items = new ArrayList<>();
+        // Load items from data file to maintain persistence
+        readItems();
 
-        // Set up the Adapter from model to RecylcerView
+        // Set up the Adapter from model to RecyclerView
         ItemsAdapter.OnLongClickListener longClickListener = new ItemsAdapter.OnLongClickListener()
         {
             @Override
@@ -41,14 +48,20 @@ public class MainActivity extends AppCompatActivity {
                  // Delete item from the model and notify the adapter
                 items.remove(position);
                 adapter.notifyItemRemoved(position);
+
                 Toast.makeText(MainActivity.this, "Item was removed", Toast.LENGTH_SHORT).show();
+
+                // save the added item to list
+                writeItems();
             }
         };
 
+        // Set up the adapter with the override function and list of items
         adapter = new ItemsAdapter(items, longClickListener);
         items_rv.setAdapter(adapter);
         items_rv.setLayoutManager(new LinearLayoutManager(this));
 
+        // Function to add a new item to the list when user click "Add"
         button_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -63,8 +76,36 @@ public class MainActivity extends AppCompatActivity {
 
                 // Pop-up message that tells user that an item has been successfully added
                 Toast.makeText(MainActivity.this, "Item was added", Toast.LENGTH_SHORT).show();
+
+                // save the added item to list
+                writeItems();
             }
         });
     }
 
+    // Get the file we want to read and write from
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    // Function to read lines from a file and populate the list of todo items
+    private void readItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    // Overwrite the data file to save changes when user add or remove an item
+    private void writeItems() {
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Error writing items", e);
+        }
+    }
 }
